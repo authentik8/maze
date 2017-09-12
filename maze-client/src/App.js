d19abcadc233d315795853265ea73308be0458dc
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-12 23:48:45 +0100

    Integrate Bootstrap

--- maze-client/src/App.js
+++ maze-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
-    BrowserRouter as Router,
-    Route
+  BrowserRouter as Router,
+  Route,
+  Link
 } from 'react-router-dom'
 import './App.css';
 
@@ -11,17 +12,17 @@ import Maze from './routes/Maze'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Maze Solver</h2>
-        </div>
-        <Router>
-          <div>
+      <Router>
+        <div>
+          <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand App-mainLink">Maze Solver</Link>
+          </div>
+          <div className="App-content container">
             <Route exact path="/" component={Home} />
             <Route path="/:width/:height" component={Maze} />
           </div>
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
