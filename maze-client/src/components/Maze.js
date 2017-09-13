96a0e6f965f0d920d21e84c4d1046eb41bb81661
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-13 18:49:20 +0100

    Add key press event listeners

--- maze-client/src/components/Maze.js
+++ maze-client/src/components/Maze.js
@@ -11,11 +11,15 @@ export class Maze extends React.Component {
     super(props)
     this.state = {
       loaded: false,
-      maze: {}
+      maze: {},
+      path: []
     }
   }
 
   componentDidMount() {
+
+    window.addEventListener('keydown', this.handleKeyPress)
+
     const { size } = this.props
     fetch(`${API_URL}/maze/${size}`)
       .then(res => {
@@ -26,20 +30,67 @@ export class Maze extends React.Component {
           return res.json()
         }
       })
-      .then(maze => this.setState({ maze, loaded: true }))
+      .then(maze => {
+        const path = maze.cells.reduce((prev, next) => [...prev, ...next]).filter(cell => !!cell.start)
+
+        console.log(path)
+
+        this.setState({ maze, path, loaded: true, eventListenerMounted: true })
+      })
+  }
+
+  componentWillUnmount() {
+    if (this.state.eventListenerMounted) {
+      window.removeEventListener('keydown', this.handleKeyPress)
+    }
+  }
+
+  handleKeyPress = e => {
+    const value = e.key
+    const { maze, path, loaded } = this.state
+
+    if (loaded && ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'].includes(value)) {
+      const current = path[path.length - 1]
+
+      const direction = value.substring(5)
+
+      // Check if invalid move
+      if (Maze.isValidMove(maze, current, direction)) {
+        console.log('Can move!')
+      } else {
+        console.log('Can\'t move!')
+      }
+
+      e.preventDefault()
+    }
+
+  }
+
+  static isValidMove(maze, current, direction) {
+
+    
+    
+    if ( // If on the boundaries, can't move outside the maze
+      (current.row === 0 && direction === 'Up') ||
+      (current.col === 0 && direction === 'Left') ||
+      (current.row === maze.rows - 1 && direction === 'Down') ||
+      (current.col === maze.columns - 1 && direction === 'Right')
+    ) {
+      return false;
+    } else {
+      return true
+    }
   }
 
   generateMaze = () => {
 
     //const { width, height } = this.props
-    const { maze } = this.state
+    const { maze, path } = this.state
 
     const rows = maze.cells.map((row, index) => (<MazeRow cells={row} key={index} />))
 
-    console.log(maze.cells)
-
     return (
-      <table className='maze'>
+      <table className='maze' onKeyPress={this.onKeyPress} tabIndex='0'>
         <tbody>
           {rows}
         </tbody>
@@ -57,7 +108,7 @@ export class Maze extends React.Component {
     } else {
       content = this.generateMaze()
     }
-
+    
     return (
       <div>
         {content}
