5a38f87454bcf9276c0a64c91a1c818342f1aa9d
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-14 08:11:45 +0100

    Move `Maze` model to class library

--- MazeAPI/Controllers/MazeController.cs
+++ MazeAPI/Controllers/MazeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MazeAPI.Models;
+using MazeLib;
 
 namespace MazeAPI.Controllers
 {
