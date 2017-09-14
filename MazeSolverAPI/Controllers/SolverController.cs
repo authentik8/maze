0e27cd3753a9d048241547f17c01118b16c15b58
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-14 07:54:07 +0100

    Add scaffold project for maze solver API

---
+++ MazeSolverAPI/Controllers/SolverController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MazeSolverAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SolverController : Controller
+    {
+        // POST api/solver
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
