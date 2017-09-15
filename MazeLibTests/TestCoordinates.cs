41341eb17352a9f73c75163484e001d838034f0f
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-15 20:05:11 +0100

    Add unit testing project for `MazeLib`

---
+++ MazeLibTests/TestCoordinates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using MazeLib;
+
+namespace MazeLib.Tests {
+    
+    public class TestCoordinates {
+
+        [Fact]
+        public void ToString() {
+            Coordinates coordinates = new Coordinates(53, 29);
+            string stringValue = coordinates.ToString();
+            Assert.Equal(stringValue, "MazeLib.Coordinates(53, 29)");
+        }
+
+        [Fact]
+        public void Equals__True() {
+            Coordinates first = new Coordinates(3, 4);
+            Coordinates second = new Coordinates(3, 4);
+            Assert.Equal(first, second);
+            Assert.True(first == second);
+        }
+
+        [Fact]
+        public void Equals__False() {
+            Coordinates first = new Coordinates(2, 3);
+            Coordinates second = new Coordinates(3, 2);
+            Assert.NotEqual(first, second);
+            Assert.True(first != second);
+        }
+
+    }
+}
