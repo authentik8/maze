455ffeb18b8095d223a2f5a28b97a60340ad6fc1
Jonathan Windridge <jonathan.windridge@gmail.com>
2017-09-15 09:57:17 +0100

    Split MazeLib components into separate files

---
+++ Maze/Coordinates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeLib
+{
+    public struct Coordinates {
+        public int Row;
+        public int Col;
+
+        public Coordinates(int row, int col) {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Coordinates))
+                return false;
+
+            Coordinates other = (Coordinates)obj;
+            return this.Row == other.Row && this.Col == other.Col;
+
+        }
+
+        public override int GetHashCode() {
+            var hashCode = 1084646500;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Col.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Coordinates coordinates1, Coordinates coordinates2) {
+            return coordinates1.Equals(coordinates2);
+        }
+
+        public static bool operator !=(Coordinates coordinates1, Coordinates coordinates2) {
+            return !(coordinates1 == coordinates2);
+        }
+    }
+}
---
+++ Maze/MazeCell.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeLib
+{
+    public class MazeCell {
+
+        public bool start { get; private set; }
+        public bool goal { get; private set; }
+
+        private Coordinates coordinates;
+
+        public int row { get => coordinates.Row; private set => coordinates.Row = value; }
+        public int col { get => coordinates.Col; private set => coordinates.Col = value; }
+        public Coordinates Coordinates { get => coordinates; }
+
+        // A true value for any of these represents a valid movement direction from this cell
+        public bool up { get; private set; }
+        public bool right { get; private set; }
+        public bool down { get; private set; }
+        public bool left { get; private set; }
+
+        public MazeCell(int row,
+                        int col) {
+            this.coordinates = new Coordinates(row, col);
+        }
+
+        [JsonConstructor]
+        public MazeCell(int row, int col, bool start, bool goal, bool up, bool right, bool down, bool left) {
+            this.coordinates = new Coordinates(row, col);
+            this.start = start;
+            this.goal = goal;
+            this.up = up;
+            this.right = right;
+            this.down = down;
+            this.left = left;
+        }
+
+        public void MakeStart() {
+            start = true;
+        }
+
+        public void MakeGoal() {
+            goal = true;
+        }
+
+        public void OpenUp() {
+            up = true;
+        }
+
+        public void OpenRight() {
+            right = true;
+        }
+
+        public void OpenDown() {
+            down = true;
+        }
+
+        public void OpenLeft() {
+            left = true;
+        }
+    }
+}
